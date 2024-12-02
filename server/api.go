f74b592968e70df31c818904d4f5cd0ec84cedb2
caffeinated92 <ahmad@signal18.io>
2024-12-02 18:37:48 +0700

    fix 404

--- server/api.go
+++ server/api.go
@@ -222,6 +222,10 @@ func (repman *ReplicationManager) apiserver() {
 		router.PathPrefix("/grafana/").Handler(http.StripPrefix("/grafana/", repman.SharedirHandler("grafana")))
 	}
 
+	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+
 	router.HandleFunc("/api/login", repman.loginHandler)
 	router.HandleFunc("/api/login-git", repman.loginHandler)
 	//router.Handle("/api", v3.NewHandler("My API", "/swagger.json", "/api"))
